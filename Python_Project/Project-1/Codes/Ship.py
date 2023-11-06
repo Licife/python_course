6f3d446162404b316be4f66ac6fa68305c4da749
Licifer <2813206264@qq.com>
2023-11-06 20:01:38 +0800

    prpject-2

--- Python_Project/Project-1/Codes/Ship.py
+++ Python_Project/Project-1/Codes/Ship.py
@@ -1,10 +1,19 @@
 import pygame
 
+
 class Ship:
     """飞船的类"""
-    def __init__(self, al_game):
-        """初始化飞船"""
-        self.screen = al_game.screen
-        self.screen_rect = al_game.screen.get_rect()
 
-        self.image = pygame.image.load('Images/ship.bmp')
+    def __init__(self, ai_game):
+        """初始化飞船"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('D:/Study/python_works/Python_Project/Project-1/Images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
--- Python_Project/Project-1/Codes/alien_invasion.py
+++ Python_Project/Project-1/Codes/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from Setting import Settings
+from Ship import Ship
 
 
 class AlienInvasion:
@@ -14,21 +15,31 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 键盘与鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
-
-            # 抹除旧屏幕 重新绘制新屏幕
-            pygame.display.flip()
             self.clock.tick(60)
+
+    def _check_events(self):
+        # 键盘与鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        # 每次循环重新绘制
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # 抹除旧屏幕 重新绘制新屏幕
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
-    al = AlienInvasion()
-    al.run_game()
+    ai = AlienInvasion()
+    ai.run_game()
